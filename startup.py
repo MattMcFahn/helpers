ad6a612457963e21657c79aa5294102e917737ce
MattMcFahn <44867810+MattMcFahn@users.noreply.github.com>
2020-10-14 16:09:07 +0100

    Initial commit

---
+++ startup.py
@@ -0,0 +1,40 @@
+"""
+Script to be run on startup to set up PYTHONPATH variable
+
+In Spyder, you can set this to be run every time Spyder is open
+1. Tools > Preferences > IPython console > Startup
+2. Under 'Run a file', browse to startup.py
+
+Can be easily customised for different setups, by varying the type of roots used
+"""
+
+import pandas as pd
+import sys
+import os
+
+### PYTHONPATH
+root = os.path.expanduser("~")
+paths = {'github_root':rf'{root}\Documents\GitHub',
+         'coding_root':rf'{root}\Documents\Coding',
+         'local_root':rf'{root}\Documents\local_pyth_config',
+         'legatum_root':rf'{root}\Documents\Legatum\centreformetrics'}
+
+# Iterate over subdirectory tree, append to path. Remove root, paths from local memory
+for root_type, path in paths.items():
+    sys.path.append(path)
+    for subdir, dirs, files in os.walk(path):
+        git_or_env_path = '\env' in subdir or '\.git' in subdir
+        if not git_or_env_path:
+            sys.path.append(subdir)
+            print(subdir)
+
+
+del root
+del paths
+
+### PANDAS preferences
+# Make dataframes easier to read in the console
+pd.set_option('max_columns',10)
+pd.set_option('expand_frame_repr',False)
+pd.set_option('precision',8)
+
