448d56dfbb499a447382d02c7ae2e80b08a7f54b
MattMcFahn <44867810+MattMcFahn@users.noreply.github.com>
2020-12-12 19:53:52 +0000

    Update startup.py
    Edit using the __name__ == '__main__' functionality to stop random variables from the for loops loading into the variable explorer if ran on startup (as intended). Due to this, no need to call "del variable" multiple times.

--- startup.py
+++ startup.py
@@ -11,30 +11,28 @@ Can be easily customised for different setups, by varying the type of roots used
 import pandas as pd
 import sys
 import os
-
-### PYTHONPATH
-root = os.path.expanduser("~")
-paths = {'github_root':rf'{root}\Documents\GitHub',
-         'coding_root':rf'{root}\Documents\Coding',
-         'local_root':rf'{root}\Documents\local_pyth_config',
-         'legatum_root':rf'{root}\Documents\Legatum\centreformetrics'}
-
-# Iterate over subdirectory tree, append to path. Remove root, paths from local memory
-for root_type, path in paths.items():
-    sys.path.append(path)
-    for subdir, dirs, files in os.walk(path):
-        git_or_env_path = '\env' in subdir or '\.git' in subdir
-        if not git_or_env_path:
-            sys.path.append(subdir)
-            print(subdir)
-
-
-del root
-del paths
-
-### PANDAS preferences
-# Make dataframes easier to read in the console
-pd.set_option('max_columns',10)
-pd.set_option('expand_frame_repr',False)
-pd.set_option('precision',8)
-
+def main():
+    ### PYTHONPATH
+    root = os.path.expanduser("~")
+    paths = {'github_root':rf'{root}\Documents\GitHub',
+             'coding_root':rf'{root}\Documents\Coding',
+             'local_root':rf'{root}\Documents\local_pyth_config',
+             'legatum_root':rf'{root}\Documents\Legatum\centreformetrics'}
+    
+    # Iterate over subdirectory tree, append to path. Remove root, paths from local memory
+    for root_type, path in paths.items():
+        sys.path.append(path)
+        for subdir, dirs, files in os.walk(path):
+            git_or_env_path = '\env' in subdir or '\.git' in subdir
+            if not git_or_env_path:
+                sys.path.append(subdir)
+    
+    
+    ### PANDAS preferences
+    # Make dataframes easier to read in the console
+    pd.set_option('max_columns',10)
+    pd.set_option('expand_frame_repr',False)
+    pd.set_option('precision',8)
+    
+if __name__ == '__main__':
+    main()
